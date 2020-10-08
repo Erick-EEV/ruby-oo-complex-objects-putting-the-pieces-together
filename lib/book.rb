b5af1eff7aa474e8985be0b62ddd238de43892a2
Erick-EEV <EEVERICKEEV@gmail.com>
2020-10-07 21:31:42 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,46 @@
 
+class Book
+    attr_accessor :author, :page_count, :genre
+    #This creates a setter AND a getter
+
+    attr_reader :title
+#This creates ONLY a getter
+
+    def initialize(title)
+        @title = title
+    end
+
+    # def title
+    #     @title
+    # end This is done in attr_reader :title
+
+    # def author=(author)
+    #     @author = author
+    # end
+
+    # def author
+    #     @author
+    # end #This is all done in attr_accessor :author
+
+    # def page_count=(page_count)
+    #     @page_count = page_count
+    # end
+
+    # def page_count
+    #     @page_count
+    # end #This is all done in :page_count
+
+    # def genre=(genre)
+    #     @genre = genre
+    # end
+
+    # def genre
+    #     @genre
+    # end #This is all done in :genre
+
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
 
